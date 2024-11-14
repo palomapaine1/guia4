e73b4160a3a7fbb9f1e999ec5ff77da5934a528d
palomapaine1 <palomapaine4@gmail.com>
2024-11-14 11:57:24 -0300

    Update untitled9.py

--- untitled9.py
+++ untitled9.py
@@ -12,3 +12,15 @@ import streamlit as st
 st.title('Mi Primera Aplicación Web con Streamlit')
 # Texto simple
 st.write('Esta es una aplicación web de ejemplo utilizando Streamlit.')
+import streamlit as st
+import pandas as pd
+# Título de la aplicación
+st.title('Aplicación de Análisis de Datos')
+# Cargar el archivo CSV
+uploaded_file = st.file_uploader('Sube tu archivo CSV', type=['csv'])
+if uploaded_file is not None:
+# Leer el archivo CSV usando Pandas
+df = pd.read_csv(uploaded_file)
+# Mostrar las primeras filas del archivo
+st.write('Primeras 5 filas del archivo:')
+st.write(df.head())
